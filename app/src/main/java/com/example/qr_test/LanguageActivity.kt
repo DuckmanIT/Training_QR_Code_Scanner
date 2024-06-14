7a3e6db616a4dcb1f72adcaf0d833e5ccc2b5513
DuckmanIT <hoangducptit@gmail.com>
2024-06-14 17:48:29 +0700

    push code

---
+++ app/src/main/java/com/example/qr_test/LanguageActivity.kt
@@ -0,0 +1,17 @@
+package com.example.qr_test
+
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import androidx.recyclerview.widget.RecyclerView
+
+class LanguageActivity : AppCompatActivity() {
+    private lateinit var binding : LanguageActivity
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+    }
+}
